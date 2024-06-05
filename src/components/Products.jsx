6db0c6a614046005a0033fe16eb1423c9f2ca412
Nobisuke19 <subham019650@gmail.com>
2024-06-05 18:45:57 +0530

    third-responsive

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,7 +6,7 @@ import Card from "./ui/Card";
 
 const Products = () => {
   return (
-      <div className=" md:w-full container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+      <div className=" md:w-full container mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
        <Card/>
        <Card/>
        <Card/>
